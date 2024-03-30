25caf423f1258862714e0946a9fba2065a95cf35
Piyush Kumar Yadav <73697893+piyush1212@users.noreply.github.com>
2024-03-30 13:27:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function validEmail(str) {
-  //your JS code here.
+    // Regular expression to validate email format
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(str);
 }
+
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
